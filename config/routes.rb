5ef397d439804f09ebf7f6f59cd05483f2e47407
TalhaNaqshbandi <muhammad.talha@activekeysolutions.com>
2026-02-20 14:25:59 +0500

    Refactor student and enrollment routes to include member and collection actions for semester promotion and enrollment requests

--- config/routes.rb
+++ config/routes.rb
@@ -8,10 +8,25 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :departments
-      resources :students
+      resources :students do
+        member do
+          patch :promote_semester
+        end
+      end
       resources :teachers
       resources :courses
-      resources :enrollments
+      resources :enrollments, except: [:destroy] do
+        member do
+          patch :approve
+          patch :reject
+          patch :complete
+          patch :drop
+          patch :withdraw
+        end
+        collection do
+          post :request_enrollment
+        end
+      end
       resources :grades
       resources :grade_items
     end
